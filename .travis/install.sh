8907abe0bcf4ec8462feb0c74b341c4ce9adf7b2
Chris Mayes <cmayes@cmay.es>
2018-10-09 19:47:55 -0400

    Forgot to conditionally install platform-specific miniconda.

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,11 +2,7 @@
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo apt-get update
-fi
-# We do this conditionally because it saves us some downloading if the
-# version is the same.
-if [[ "$PYTHON_VER" == "2.7" ]]; then
     wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
 else
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh;
 fi
