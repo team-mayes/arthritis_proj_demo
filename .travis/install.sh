5139fccc5f21796cc0bec5280da22adcc6b595b1
Chris Mayes <cmayes@cmay.es>
2018-10-09 19:33:37 -0400

    Trying to get proper pytest/conda env w/ script

--- .travis/install.sh
+++ .travis/install.sh
@@ -10,18 +10,3 @@ if [[ "$PYTHON_VER" == "2.7" ]]; then
 else
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 fi
-
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-# Useful for debugging any issues with conda
-conda info -a
-
-conda create -q -n test-environment python=$PYTHON_VER pip pytest pytest-cov
-source activate test-environment
-python setup.py install
-
-# Install pip only modules
-pip install codecov
